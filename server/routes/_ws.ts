b2dad88d6ed033bbb9e570b55f191e35c84d1810
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-06-06 17:30:10 +0330

    Add comments and delete unused things from websocket route

--- server/routes/_ws.ts
+++ server/routes/_ws.ts
@@ -1,25 +1,26 @@
 const storage = useStorage("cache:state")
 
 export default defineWebSocketHandler({
-  close(peer, details) {
+  close(peer) {
     peer.unsubscribe("state-updates")
   },
-  error(peer, error) {
-      
-  },
-
   open(peer) {
     peer.subscribe("state-updates")
   },
   async message(peer, message) {
     console.log(`Websocket message:`, message);
     
+    // Read data from message
     const blob = message.text()
     const data = JSON.parse(blob)
 
+    // Save data from message to storage
     await storage.setItem(data.id, data.value)
+
     // const data = await readBody(event)
     // event.waitUntil(storage.setItem(id, data))
+
+    // Publish data to all subscribers
     peer.publish("state-updates", data)
   },
 })

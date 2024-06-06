7f67e84379cccc3a59a2382d70870a31a8e393bb
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-06-06 17:35:25 +0330

    Writing comments in composables

--- composables/shared-state.ts
+++ composables/shared-state.ts
@@ -21,40 +21,44 @@ export const useSharedState = <T extends Record<string, any>>(id = "state") => {
   if(import.meta.client) {
     let snapshot = hash(state.value)
 
+    // Initilize websocket client
     const ws = useWebsocket()
 
     // Send new state changes to server
     const syncState = async (newState: T) => {
+      // Check if websocket ready
       await ws?.ready
 
-      console.log("ready");
-      
-
+      // Send new state to all subscribers and server using websocket
       ws?.ws.send(JSON.stringify({id, value: newState}))
 
+      // Send new state to server
       // await $fetch(`/api/state/${id}`, {
       //   method: "PUT",
       //   body: newState
       // })
     }
 
+    // On recieve any message
     ws?.ws.addEventListener("message", event => {
       const data = JSON.parse(event.data)
 
-      console.log(data);
-      
-
+      // Check the cache id
       if(data.id === id) {
         const newSnapshot = hash(data.value)
 
+        // Chech if data hash is as the same as previous data (Avoid duplication)
         if(newSnapshot !== snapshot) {
+          // Set a new data hash
           snapshot = newSnapshot
 
+          // Update state
           state.value = data.value
         }
       }
     })
     
+    // Watch deeply if state changes
     watch(state, syncState, {deep: true, flush: "post"})
   }
 
--- composables/websocket.ts
+++ composables/websocket.ts
@@ -1,13 +1,20 @@
 let ws: WebSocket
 
 export const useWebsocket = () => {
+
+  // Don't do anything on server 
   if(import.meta.server) return
 
+  // If websocket didnt initlized before
   if(!ws) {
+    // Find websocket protocol from user location 
     const protocol = location.protocol === 'https:' ? 'wss' : 'ws'
+
+    // Lnitilize websocket client
     ws = new WebSocket(`${protocol}://${location.host}/_ws`)
   }
 
+  // Return websocket and ready promise
   return {
     ws, ready: new Promise((resolve) => {
       ws.addEventListener("open", resolve)
